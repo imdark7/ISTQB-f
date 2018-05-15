43a187399e64d29bdb90c3b80814d2ff666f8463
Grigory Grib <imdark@yandex.ru>
2018-05-15 22:22:55 +0500

    Еще немного прекрасного

--- ISTQB-f/Scripts/Components/SettingsLink.jsx
+++ ISTQB-f/Scripts/Components/SettingsLink.jsx
@@ -1,13 +1,13 @@
 ﻿class SettingsLink extends React.Component {
     render() {
-        var handler = this.props.type === "lang" ? this.props.langHandler() : this.props.randHandler();
+        var handler = this.props.type === "lang" ? () => this.props.langHandler() : () => this.props.randHandler();
         var name = this.props.type === "lang"
             ? "translation"
             : (this.props.btnText === "Случайные вопросы" ? "shuffle" : "timeline");
         return (
             <div>
-                <a onClick={() => handler}>{this.props.btnText}</a>
-                <img id="order-set" src={`/Content/${name}.png`} height="45" width="45" style={{ display: "none" }} onClick={() => handler} />
+                <a onClick={handler}>{this.props.btnText}</a>
+                <img id="order-set" src={`/Content/${name}.png`} height="45" width="45" style={{ display: "none" }} onClick={handler} />
             </div>
         );
     }
